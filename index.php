1ccd6f9d02c3b16395c665b6796e8eb8d60165c0
Zenon <veresovdanil@gmail.com>
2025-03-10 21:18:44 +0300

    feat: css style change

--- index.php
+++ index.php
@@ -15,20 +15,28 @@ $response = $client->request('GET', 'https://www.world-wonders-api.org/v0/wonder
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/x-icon" href="/image/pin.png">
+    <link rel="stylesheet" href="./style/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
     <title>Wonders World</title>
 </head>
 <body>
-<header>
-    <h1>Wonders World</h1>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="wonders.php">Wonders</a></li>
+<header class="header">
+    <div class="header-logo"><a href="index.php/">Wonders World</a></div>
+
+    <nav class="nav">
+        <ul class="nav__container">
+            <li class="nav__container__item"><a class="container__item-link" href="index.php">Home</a></li>
+            <li class="nav__container__item"><a class="container__item-link" href="wonders.php">Wonders</a></li>
         </ul>
     </nav>
 </header>
-<main>
-    <ul>
+<main class="hero">
+    <h1>Wonders of the World</h1>
+    <div class="container">
     <?php
 
     $wonders = json_decode($response->getBody(), true);
@@ -40,12 +48,12 @@ $response = $client->request('GET', 'https://www.world-wonders-api.org/v0/wonder
         $name = $wonder['name'];
         $time_period = $wonder['time_period'];
         $name_wonders = str_replace(' ', '-', strtolower($name));
-        echo "<li>
-<img src='{$image}' alt='' style='width: 200px; height: 200px'>
-<a href='https://www.world-wonders-api.org/v0/wonders/name/{$name_wonders}'>$name: $time_period</a></li>";
+        echo "<div class='container__item'>
+<img class='container__item__img' src='{$image}' alt=''><br>
+<a href='https://www.world-wonders-api.org/v0/wonders/name/{$name_wonders}'>$name</a></div>";
     }
     ?>
-    </ul>
+        </div>
 </main>
 </body>
 </html>
